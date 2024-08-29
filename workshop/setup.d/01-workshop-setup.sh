e8d2e03937ee54eacd27dd8c72ad4c2d39203c39
Timo Salm <accounts@timosalm.de>
2024-08-29 12:02:22 +0200

    Improvements

--- workshop/setup.d/01-workshop-setup.sh
+++ workshop/setup.d/01-workshop-setup.sh
@@ -5,6 +5,5 @@ set -eo pipefail
 
 jq ". + { \"editor.fontSize\": 14, \"files.exclude\": { \".**\": true}}" /home/eduk8s/.local/share/code-server/User/settings.json | sponge /home/eduk8s/.local/share/code-server/User/settings.json
 
-git clone https://github.com/timosalm/spring-petclinic-2.7 
-(cd spring-petclinic-2.7 && git remote rename origin upstream && git remote add origin $GIT_PROTOCOL://$GIT_HOST/spring-petclinic && while ! git push origin main; do sleep 5; done)
-rm -rf spring-petclinic-2.7
+
+(cd /opt/git/repositories && git clone --bare https://github.com/timosalm/spring-petclinic-2.7 spring-petclinic
